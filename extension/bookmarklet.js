dd1954fbf42a9e8461c1f55b6d16c499c41bf7ec
Tarzan <83577193+adithya-r-prabhu@users.noreply.github.com>
2021-11-28 22:10:39 +0530

    extension uploaded

---
+++ extension/bookmarklet.js
@@ -0,0 +1,13 @@
+(function(){var a= document.getElementsByClassName("w3-btn w3-margin-bottom");let i = a.length;for (let i = 0; i < 10; i++) {  var b= document.getElementsByClassName("w3-btn w3-margin-bottom")[0].href;var c = ('<iframe width="100%" height="600px" src="'+ b +'"</iframe>');document.getElementsByClassName("w3-example")[0].outerHTML =  c;}})();
+
+
+
+
+
+
+
+
+
+
+
+
